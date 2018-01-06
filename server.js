479c90dff53adc77a7584b285ee1e18ab3921982
benice195 <szczyrbaw@gmail.com>
2018-01-06 19:50:41 +0100

    Pug początki

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+let app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use('/store', (req, res, next) => {
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.get('/store', (req, res) => {
+    res.send('To jest sklep');
+});
+
+app.get('/first-template', (req, res) => {
+    res.render('first-template');
+});
+
+app.get('/dynamic-view', (req, res) => {
+    res.render('dynamic', {
+        name: "Moja dynamiczna strona",
+        url: "http://www.google.com"
+    });
+});
+
+app.get('/google-log', (req, res) => {
+    res.render('google-login', {
+        info: "Tutaj zalogujesz do go swojego Google",
+        infobutton: "Kliknij",
+        name: "Logowanko do google",
+        url: "/auth/google"
+    });
+});
+
+app.get('/auth/google', (req,res) => {
+    res.render('google-form');
+});
+
+app.listen(3000);
+app.use((req, res, next) => {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
